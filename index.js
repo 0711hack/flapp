9b76d4326cf68e3ba218a3e0104fd5bad456547b
Andreas Wittig <andreas@widdix.de>
2015-11-07 17:14:23 +0100

    push notifications

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var Tinkerforge = require('tinkerforge');
+var uuid = require('node-uuid');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+var fs = require('fs');
+var exec = require('child_process').exec;
+var https = require('https');
 
 var HOST = 'localhost';
 var PORT = 4223;
@@ -6,8 +12,8 @@ var PORT = 4223;
 var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
 var piezo = new Tinkerforge.BrickletPiezoSpeaker("sAk", ipcon); // Create device object
 var motionDetector = new Tinkerforge.BrickletMotionDetector("sKj", ipcon);
-var servo = new Tinkerforge.BrickServo("6kM6oJ", ipcon)
-var nfcRFID = new Tinkerforge.BrickletNFCRFID("uw2", ipcon)
+var servo = new Tinkerforge.BrickServo("6kM6oJ", ipcon);
+var nfcRFID = new Tinkerforge.BrickletNFCRFID("uw2", ipcon);
 
 var tagType = 0;
 
@@ -17,6 +23,62 @@ ipcon.connect(HOST, PORT,
     }
 ); // Connect to brickd
 // Don't use device before ipcon is connected
+
+function uploadImageToS3() {
+    var id = uuid.v4();
+    exec("fswebcam -c /home/tf/webcam.conf --save=/tmp/flapp.jpg", function (error, stdout, stderr) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        } else {
+            fs.readFile('/tmp/flapp.jpg', function (err, data) {
+                if (err) throw err;
+                console.log(data);
+                var params = {Bucket: '0711hack-flapp', Key: id, Body: data};
+                s3.putObject(params, function(err, data) {
+                    console.log(err, data);
+                });
+            });
+        }
+    });
+    return id;
+}
+
+function createKnock(imageId) {
+    var postData = JSON.stringify({
+        "image": imageId
+    });
+
+    var options = {
+        hostname: '1rzcudhqjl.execute-api.eu-west-1.amazonaws.com',
+        port: 443,
+        path: '/dev/flap/6fd83fda-9b74-477f-9099-9a92d7bda30f/knock',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': postData.length
+        }
+    };
+    
+    var req = https.request(options, function(res) {
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+        res.on('end', function() {
+            console.log('No more data in response.')
+        })
+    });
+
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+
+    // write data to request body
+    req.write(postData);
+    req.end();
+}
 
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function (connectReason) {
@@ -40,6 +102,7 @@ servo.on(Tinkerforge.BrickServo.CALLBACK_POSITION_REACHED,
                 servo.setVelocity(0, 20000);
                 servo.setPosition(0, -9000);
                 servo.enable(0);
+                createKnock(uploadImageToS3());
             }, 5000)
         } else if (position === -9000) {
             servo.disable(servoNum);
--- lambda.js
+++ lambda.js
@@ -1,7 +1,7 @@
 var AWS = require('aws-sdk');
 var db = new AWS.DynamoDB();
+var sns = new AWS.SNS({region: "us-east-1"});
 var uuid = require('node-uuid');
-
 
 function createFlap(event, context) {
     var uid = uuid.v4();
@@ -26,6 +26,7 @@ function createFlap(event, context) {
 function createKnock(event, context) {
     var flap = event.flapid;
     var timestamp = Date.now();
+    var image = event.body.image;
     var params = {
         "Item": {
             "flap": {
@@ -33,17 +34,25 @@ function createKnock(event, context) {
             },
             "timestamp": {
                 "N": timestamp.toString()
+            },
+            "image": {
+                "S": image
+            },
+            "state": {
+                "S": "CREATED"
             }
         },
         "TableName": "flapp-knock"
     };
     console.log(params);
-    db.putItem(params, function(err) {
-        if (err) {
-            context.fail(err);
-        } else {
-            context.succeed({"body": {"flap": flap, "timestamp": timestamp}});
-        }
+    sendPushNotification(flap, timestamp, function() {
+        db.putItem(params, function(err) {
+            if (err) {
+                context.fail(err);
+            } else {
+                context.succeed({"body": {"flap": flap, "timestamp": timestamp}});
+            }
+        });    
     });
 }
 
@@ -70,6 +79,31 @@ function getKnock(event, context) {
         }
     });
 }
+
+function sendPushNotification(flap, timestamp, cb) {
+    console.log("SEND PUSH NOTIFICATION START");
+    var params = { 
+        Message: JSON.stringify({
+            "default": JSON.stringify({
+                "flap": flap, 
+                "timestamp": timestamp    
+            })
+        }), /* required */
+        MessageStructure: 'json',
+        TopicArn: 'arn:aws:sns:us-east-1:004794452776:flapp_alldevices_MOBILEHUB_1607389038'
+    };
+    sns.publish(params, function(err, data) {
+        if (err) {
+            cb(err);
+            console.log(err, err.stack); // an error occurred
+        } else {
+            console.log("SEND PUSH NOTIFICATION SUCCESS");
+            cb(undefined, data);
+        }
+    });
+}
+
+sendPushNotification("test", "test", function() {});
 
 exports.handler = function(event, context) {
     console.log(JSON.stringify(event));
