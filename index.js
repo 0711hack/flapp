0137ceb1b3e83866db8777ab859c7b6489afce73
Andreas Wittig <andreas@widdix.de>
2015-11-08 10:49:53 +0100

    polling state from server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var s3 = new AWS.S3();
 var fs = require('fs');
 var exec = require('child_process').exec;
 var https = require('https');
+var restify = require('restify');
 
 var HOST = 'localhost';
 var PORT = 4223;
@@ -18,6 +19,11 @@ var nfcRFID = new Tinkerforge.BrickletNFCRFID("uw2", ipcon);
 var isNotMyCat = false;
 
 var tagType = 0;
+
+var apiClient = restify.createJsonClient({
+    url: 'https://1rzcudhqjl.execute-api.eu-west-1.amazonaws.com:443',
+    version: '*'
+});
 
 ipcon.connect(HOST, PORT,
     function (error) {
@@ -35,7 +41,11 @@ function uploadImageToS3() {
             fs.readFile('/tmp/flapp.jpg', function (err, data) {
                 if (err) throw err;
                 console.log(data);
-                var params = {Bucket: '0711hack-flapp', Key: id, Body: data};
+                var params = {
+                    Bucket: '0711hack-flapp', 
+                    Key: id,
+                    Body: data
+                };
                 s3.putObject(params, function(err, data) {
                     console.log(err, data);
                 });
@@ -45,46 +55,51 @@ function uploadImageToS3() {
     return id;
 }
 
-function createKnock(imageId) {
-    var postData = JSON.stringify({
-        "image": imageId
+function createKnock(flap, imageId) {
+    apiClient.post('/dev/v1/flap/' + flap + '/knock', {"image": imageId}, function(err, req, res, obj) {
+        console.log("CREATE KNOCK");
+        console.log('%d -> %j', res.statusCode, res.headers);
+        console.log('%j', obj);
+        checkKnock(obj.flap, obj.id, 0)
     });
-
-    var options = {
-        hostname: '1rzcudhqjl.execute-api.eu-west-1.amazonaws.com',
-        port: 443,
-        path: '/dev/flap/6fd83fda-9b74-477f-9099-9a92d7bda30f/knock',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': postData.length
-        }
-    };
-
-    var req = https.request(options, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-        res.on('end', function() {
-            console.log('No more data in response.');
-        });
-    });
-
-    req.on('error', function(e) {
-        console.log('problem with request: ' + e.message);
-    });
-
-    // write data to request body
-    req.write(postData);
-    req.end();
 }
+
+function checkKnock(flap, knock, checks) {
+    console.log("checkKnock");
+    apiClient.get('/dev/v1/flap/' + flap + '/knock/' + knock, function (err, req, res, obj) {
+        checks = checks + 1;
+        console.log('Server returned: %j', obj);
+        if (obj && obj.state === "APPROVED") {
+            openDoor();
+        } else if (obj && obj.state === "DECLINED") {
+            beep();
+        } else {
+            if (checks < 1200) {
+                setTimeout(function() {
+                    checkKnock(flap, knock, checks)
+                }, 500);
+            }
+        }
+    });
+}
+
+function beep() {
+    piezo.beep(2000, 1000);
+}
+
+function openDoor() {
+    servo.setPosition(0, 9000);
+    servo.enable(0);
+}
+
+function closeDoor() {
+    servo.setPosition(0, -9000);
+    servo.enable(0);
+}
+
 
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function (connectReason) {
-        // Make 2 second beep with a frequency of 1kHz
         servo.enablePositionReachedCallback();
         servo.setVelocity(0, 20000);
         servo.setPulseWidth(0, 1000, 2500);
@@ -93,11 +108,10 @@ ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
             isNotMyCat = true;
             setTimeout(function(){
                 if(isNotMyCat === true) {
-                    piezo.beep(2000, 1000);
+                    beep();
                 }
                 isNotMyCat = false;
             }, 5000);
-            //piezo.beep(2000, 1000);
         });
 
         nfcRFID.requestTagID(Tinkerforge.BrickletNFCRFID.TAG_TYPE_TYPE2);
@@ -108,10 +122,7 @@ servo.on(Tinkerforge.BrickServo.CALLBACK_POSITION_REACHED,
     function (servoNum, position) {
         if (position === 9000) {
             setTimeout(function() {
-                servo.setVelocity(0, 20000);
-                servo.setPosition(0, -9000);
-                servo.enable(0);
-                createKnock(uploadImageToS3());
+                closeDoor();
             }, 5000);
         } else if (position === -9000) {
             servo.disable(servoNum);
@@ -122,23 +133,18 @@ servo.on(Tinkerforge.BrickServo.CALLBACK_POSITION_REACHED,
 nfcRFID.on(Tinkerforge.BrickletNFCRFID.CALLBACK_STATE_CHANGED,
     // Callback function for state changed callback
     function (state, idle) {
-        if(idle) {
+        if (idle) {
             tagType = (tagType + 1) % 3;
             nfcRFID.requestTagID(tagType);
         }
-
-        if(state == Tinkerforge.BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY) {
+        if (state == Tinkerforge.BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY) {
             nfcRFID.getTagID(
                 function (tagType, tidLength, tid) {
                     // our cat 4, 6, 6, 90, 100, 52, 133
                     if (tid[0] === 4 && tid[1] === 6 && tid[2] === 6 && tid[3] === 90 && tid[4] === 100 && tid[5] === 52 && tid[6] === 133) {
                         isNotMyCat = false;
-                        servo.setPosition(0, 9000);
-                        servo.enable(0);
-                    } else {
-                        piezo.beep(2000, 2000);
+                        createKnock("10a677fb-6847-4596-95a7-325d3ffe7077", uploadImageToS3());
                     }
-                    console.log(tid);
                 },
                 function (error) {
                     console.log('Error: ' + error);
@@ -148,8 +154,6 @@ nfcRFID.on(Tinkerforge.BrickletNFCRFID.CALLBACK_STATE_CHANGED,
     }
 );
 
-
-console.log('Press key to exit');
 process.stdin.on('data',
     function (data) {
         ipcon.disconnect();
